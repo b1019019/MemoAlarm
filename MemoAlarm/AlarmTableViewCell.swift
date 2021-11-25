6bad190f4b120cb069ebbff0caf2478014fccecf
hebereke <antny0202@gmail.com>
2021-11-25 23:18:38 +0900

    ・Storyboard上にalarmTableViewを作成 ・alarmTableViewのDelegate,Datasourceの処理を仮書き

---
+++ MemoAlarm/AlarmTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlarmTableViewCell.swift
+//  MemoAlarm
+//
+//  Created by 山田純平 on 2021/11/25.
+//
+
+import UIKit
+
+class AlarmTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
