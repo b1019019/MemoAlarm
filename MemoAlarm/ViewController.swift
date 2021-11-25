aad0af5ec18566a2fc4a7cf6f5e4cdea2abe2e5f
hebereke <antny0202@gmail.com>
2021-11-25 23:35:28 +0900

    ・ストーリーボードにalarmTableViewを追加 ・UITableViewDelegate,UITableViewDatasourceの処理を仮書き

---
+++ MemoAlarm/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  MemoAlarm
+//
+//  Created by 山田純平 on 2021/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private struct Const {
+        static let alarmTableViewCellNibName = ""
+        static let alarmTableViewCellReuseIdentifier = ""
+        static let numberOfRowsInSection = 20
+    }
+    
+    @IBOutlet private weak var alarmTableView: UITableView! {
+        didSet {
+            alarmTableView.delegate = self
+            alarmTableView.dataSource = self
+            let nib = UINib(nibName: Const.alarmTableViewCellNibName, bundle: nil)
+            alarmTableView.register(nib, forCellReuseIdentifier: Const.alarmTableViewCellReuseIdentifier)
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Const.numberOfRowsInSection
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Const.alarmTableViewCellNibName, for: indexPath) as! AlarmTableViewCell
+        return cell
+    }
+    
+    
+}
