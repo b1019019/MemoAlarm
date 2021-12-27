0395a0edbad4c60280279481ed83ba186965d855
hebereke <antny0202@gmail.com>
2021-12-27 18:12:21 +0900

    EditAlarmViewController.swiftを作成、EditAlarmViewController.xibを作成

---
+++ MemoAlarm/EditAlarmViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EditAlarmViewController.swift
+//  MemoAlarm
+//
+//  Created by 山田純平 on 2021/12/27.
+//
+
+import UIKit
+
+class EditAlarmViewController: UIViewController {
+
+    @IBOutlet weak var editNameTextField: UITextField!
+    @IBOutlet weak var editRingTimingDatePicker: UIDatePicker!
+    @IBOutlet weak var editRepeatDatesView: UIView!
+    @IBOutlet weak var editNoteTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
