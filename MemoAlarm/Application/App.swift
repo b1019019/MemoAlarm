4bda727f60bcf0379b32090f6e1e9e441d92e048
hebereke <antny0202@gmail.com>
2022-02-18 11:31:49 +0900

    Appファイルの追加

---
+++ MemoAlarm/Application/App.swift
@@ -0,0 +1,23 @@
+//
+//  App.swift
+//  MemoAlarm
+//
+//  Created by 山田純平 on 2022/02/18.
+//
+
+import Foundation
+import UIKit
+
+final class App {
+    static let shared = App()
+    
+    func startInterface(in window: UIWindow) {
+        //"Main"はGlobalConstなどで定義
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationVC = storyboard.instantiateInitialViewController() as! UINavigationController
+        let viewController = navigationVC.viewControllers[0] as! ViewController
+        let mainViewModel = MainViewModel(database: UserDefaultsDatabase(), notificationManager: UserNotificationManager(), navigator: MainViewNavigator(navigationVC))
+        
+        
+    }
+}
